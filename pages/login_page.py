08498a08d8fe3293e341f7cf4a0e8fac4ae4ba26
Kubasov Alexei <kubasovalexei@yandex.ru>
2023-03-21 18:59:27 +0700

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,7 @@
 import selenium
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from base.base_class import Base
 
 class Login_page(Base):
