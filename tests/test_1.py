c2b4d2c1a03684718051e0259fcc4c3118920808
Kubasov Alexei <kubasovalexei@yandex.ru>
2023-03-21 18:58:29 +0700

    Update test_1.py

--- tests/test_1.py
+++ tests/test_1.py
@@ -8,12 +8,11 @@ from pages.login_page import Login_page
 
 users = ['standard_user', 'locked_out_user', 'problem_user', 'performance_glitch_user']
 password = 'secret_sauce'
-class All_users_authorization_test():
-    def auth(self, users, pswrd):
-        s = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=s)
-        login_page = Login_page(driver)
-        login_page.authorization(users=users, password=pswrd)
 
-test_1 = All_users_authorization_test()
-test_1.auth(users, password)
+def test_auth_test():
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s)
+    login_page = Login_page(driver)
+    login_page.authorization(users=users, password=password)
+
+#auth(users, password)
