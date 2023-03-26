757bc59ac7a51394f06b1c39bcd8cbd448266198
Kubasov Alexei <kubasovalexei@yandex.ru>
2023-03-26 17:02:12 +0700

    Update test_1.py

--- tests/test_1.py
+++ tests/test_1.py
@@ -3,8 +3,11 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
+from pages.cart_page import Cart_page
 from pages.login_page import Login_page
 from pages.main_page import Main_page
+from pages.user_information_page import User_information_page
 
 users = ['standard_user', 'locked_out_user', 'problem_user', 'performance_glitch_user']
 password = 'secret_sauce'
@@ -16,5 +19,9 @@ def test_auth_test():
     login_page.authorization(user=users[0], password=password)
     mp = Main_page(driver)
     mp.add_sauce_labs_backpack_to_cart()
+    cp = Cart_page(driver)
+    cp.click_on_checkout_button()
+    uip = User_information_page(driver)
+    uip.input_user_information()
 
 #auth(users, password)
