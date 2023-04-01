7cb67d5e951c14d1f5d33f9b2f741c16b9d7c2ac
Kubasov Alexei <kubasovalexei@yandex.ru>
2023-04-01 15:56:02 +0700

    Add second test (click on about link)

--- pages/main_page.py
+++ pages/main_page.py
@@ -15,6 +15,8 @@ class Main_page(Base):
     # Locators
     loc_sauce_labs_backpack_add_button = 'add-to-cart-sauce-labs-backpack'
     loc_shopping_cart_button = 'shopping_cart_container'
+    loc_menu_button = '//button[@id="react-burger-menu-btn"]'
+    loc_about_link = '//a[@id="about_sidebar_link"]'
 
     # Getters
     def get_sauce_labs_backpack_add_button(self):
@@ -22,6 +24,13 @@ class Main_page(Base):
 
     def get_shopping_cart_button(self):
         return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, self.loc_shopping_cart_button)))
+
+    def get_menu_button(self):
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, self.loc_menu_button)))
+
+    def get_about_link(self):
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, self.loc_about_link)))
+
     # Actions
     def add_sauce_labs_backpack_to_cart(self):
         self.get_current_url()
@@ -29,3 +38,18 @@ class Main_page(Base):
         print('Click on "Add to cart" button')
         self.get_shopping_cart_button().click()
         print('Open the shopping cart')
+
+    def click_on_menu_button(self):
+        self.get_menu_button().click()
+        print('Click on Menu button')
+
+    def click_on_about_link(self):
+        self.get_about_link().click()
+        print('Click on About link')
+
+    # Methods
+    def open_about_link(self):
+        self.get_current_url()
+        self.click_on_menu_button()
+        self.click_on_about_link()
+        self.assert_url('https://saucelabs.com/')
